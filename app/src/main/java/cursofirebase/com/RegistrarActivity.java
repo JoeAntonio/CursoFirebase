e3e3cc5ca7d0cb18f4da6e7aabde0d3aa22ea053
JoeAntonio <joeantonio36@gmail.com>
2019-01-12 17:25:29 -0600

    first commit

---
+++ app/src/main/java/cursofirebase/com/RegistrarActivity.java
@@ -0,0 +1,13 @@
+package cursofirebase.com;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RegistrarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.registrar_activity);
+    }
+}
